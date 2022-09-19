27330c7acf928ff8c7734b42880e337ccfe73b37
anisa alfauziah <75170871+anisaalfauziah2@users.noreply.github.com>
2022-09-19 14:30:57 +0700

    Initial commit

---
+++ app/Controllers/Komik.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\KomikModel;
+
+class Komik extends BaseController
+{
+    protected $komikModel;
+
+    public function __construct()
+    {
+
+        $this->komikModel = new KomikModel();
+    }
+
+    public function index()
+    {
+        // $komik = $this->komikModel->findAll();
+
+        $data = [
+            'title' => 'Daftar Komik',
+            'komik' => $this->komikModel->getKomik()
+        ];
+
+        return view('komik/index', $data);
+    }
+
+    public function detail($slug)
+    {
+        $data = [
+            'title' => 'Detail Komik',
+            'komik' => $this->komikModel->getKomik($slug)
+        ];
+        if (empty($data['komik'])) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException(' Judul komik ' . $slug . ' tidak
+            ditemukan.');
+        }
+        return view('komik/detail', $data);
+    }
+
+    public function create()
+    {
+        // session();
+        $data = [
+            'title' => 'Form Tambah Data Komik',
+            'validation' => \Config\Services::validation()
+        ];
+
+        return view('komik/create', $data);
+    }
+    public function save()
+    {
+        if (!$this->validate([
+            'judul' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} komik harus di isi.'
+                ]
+            ],
+            'sampul' => [
+                'rules' => 'uploaded[sampul]|max_size[sampul,1024]|is_image[sampul]|mime_in[sampul,image/jpg,image/jpeg,image/png]',
+                'errors' => [
+                    'uploaded' => 'Pilih gambar sampul terlebih dahulu',
+                    'max_size' => 'Ukuran gambar terlalu besar',
+                    'is_image' => 'Yang anda pilih bukan gambar',
+                    'mime_in' => 'Yang anda pilih bukan gambar'
+                ]
+            ]
+        ])) {
+            // $validation = \Config\Services::validation();
+            // return redirect()->to('/komik/create')->withInput()->with('validation', $validation);
+            return redirect()->to('/komik/create')->withInput();
+        }
+        $slug = url_title($this->request->getVar('judul'), '-', true);
+        $this->komikModel->save([
+            'judul' => $this->request->getVar('judul'),
+            'slug' => $slug,
+            'penulis' => $this->request->getVar('penulis'),
+            'penerbit' => $this->request->getVar('penerbit'),
+            'sampul' => $this->request->getVar('sampul')
+
+        ]);
+
+        session()->setFlashdata('pesan', 'Data berhasil ditambahkan');
+
+        return redirect()->to('/komik');
+    }
+    public function delete($id)
+    {
+        $this->komikModel->delete($id);
+        session()->setFlashdata('pesan', 'Data berhasil dihapus');
+        return redirect()->to('/komik');
+    }
+
+
+    public function edit($slug)
+    {
+        $data = [
+            'title' => 'Form Tambah Ubah Komik',
+            'validation' => \Config\Services::validation(),
+            'komik' => $this->komikModel->getKomik($slug)
+        ];
+
+        return view('komik/edit', $data);
+    }
+
+    public function update($id)
+    {
+        if (!$this->validate([
+            'judul' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} komik harus di isi.'
+                ]
+            ]
+        ])) {
+            $validation = \Config\Services::validation();
+            return redirect()->to('/komik/edit/' . $this->request->getVar('slug'))->withInput()->with('validation', $validation);
+        }
+        $slug = url_title($this->request->getVar('judul'), '-', true);
+        $this->komikModel->save([
+            'id' => $id,
+            'judul' => $this->request->getVar('judul'),
+            'slug' => $slug,
+            'penulis' => $this->request->getVar('penulis'),
+            'penerbit' => $this->request->getVar('penerbit'),
+            'sampul' => $this->request->getVar('sampul')
+
+        ]);
+
+        session()->setFlashdata('pesan', 'Data berhasil diubah');
+
+        return redirect()->to('/komik');
+    }
+}
---
+++ app/Views/pages/about.php
@@ -0,0 +1,13 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Perferendis quaerat molestiae ut repellat excepturi veniam veritatis quia harum sed. Error consequuntur sunt aperiam aliquid officiis nihil magni libero earum asperiores.</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
